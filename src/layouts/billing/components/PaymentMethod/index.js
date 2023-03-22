9284e0da765fd893313f63fe8707c863c9a0f64d
Salma Mrabet <103336645+salma-mrabet@users.noreply.github.com>
2023-03-22 13:39:57 +0100

    first commit

---
+++ src/layouts/billing/components/PaymentMethod/index.js
@@ -0,0 +1,42 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v4.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Icon from "@mui/material/Icon";
+import Tooltip from "@mui/material/Tooltip";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftButton from "components/SoftButton";
+
+// Soft UI Dashboard React base styles
+import borders from "assets/theme/base/borders";
+
+// Images
+import masterCardLogo from "assets/images/logos/mastercard.png";
+import visaLogo from "assets/images/logos/visa.png";
+
+function PaymentMethod() {
+  const { borderWidth, borderColor } = borders;
+
+  return (
+   <h4></h4>
+  );
+}
+
+export default PaymentMethod;
